ad786762fbbccdf8ed8f1ff5957a588ce1a1da96
Emmanuel Adeleke <emi_emi@hotmail.co.uk>
2015-07-31 01:58:03 +0100

    Referenced objects from XML file in Java

--- app/src/main/java/com/example/emmanueladeleke/loginregister/Login.java
+++ app/src/main/java/com/example/emmanueladeleke/loginregister/Login.java
@@ -5,6 +5,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -13,6 +15,9 @@ public class Login extends ActionBarActivity {
     TextView tvTitle;
     Typeface titleFont;
 
+    EditText etUsername, etPassword;
+    Button bConnect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +25,12 @@ public class Login extends ActionBarActivity {
 
         tvTitle = (TextView) findViewById(R.id.tvTitle);
 
-
         titleFont = Typeface.createFromAsset(getAssets(), "fonts/titleFont.ttf");
         tvTitle.setTypeface(titleFont);
 
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bConnect = (Button) findViewById(R.id.bConnect);
     }
 
 }
